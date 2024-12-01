e13cc2221f7d7ac249362eebc680cf38115d62e8
LT <luca.tomasinoj@gmail.com.com>
2024-12-01 10:37:24 +0100

    Init console application to enable easy debugging of every solution as i write it

---
+++ LeetCodeSolutions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LeetCodeSolutions.Problems;
+
+namespace LeetCodeSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "()[]{}";
+            bool solution = false;
+
+            // Chiama il metodo Solution (modifica il nome della classe se necessario)
+            solution = _20_ValidParentheses_Easy.Solution(s);
+
+            Console.WriteLine($"La soluzione è: {solution}");
+        }
+    }
+}
